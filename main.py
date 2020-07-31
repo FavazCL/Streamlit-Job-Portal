b7936d1bb2acded98bcf0823482fa5722839ce63
unknown <favazcl@gmail.com>
2020-07-30 20:49:53 -0400

    update hour

--- main.py
+++ main.py
@@ -164,7 +164,7 @@ def chiletrabajos():
         branch = "master"
     )
     
-schedule.every().day.at('18:05').do(chiletrabajos)
+schedule.every().day.at('21:00').do(chiletrabajos)
 
 while True:
     schedule.run_pending()
