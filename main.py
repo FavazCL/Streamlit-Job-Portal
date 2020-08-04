8d8a1b53f33b22fa61bf582a242be1dbffaac5f0
unknown <favazcl@gmail.com>
2020-08-04 14:35:42 -0400

    add new job, and add exception to laborum job

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 from numpy import array
 from datetime import datetime
-import json
+from datetime import date
 import time
+import json
 import schedule
 import os
 from github import Github
@@ -54,7 +55,6 @@ def chiletrabajos():
 
     # Obtenemos todos los links de las paginaciones del dia actual.
     driver.get(firstPage)
-
     content = driver.page_source
     soup = BeautifulSoup(content, "html.parser")
 
@@ -81,7 +81,7 @@ def chiletrabajos():
                 links.append(tmp_link.get('href'))
 
         time.sleep(10)
-
+    print('First step: Get all links - OK')
     # Eliminamos links duplicados
     links = list(set(links))
 
@@ -137,7 +137,7 @@ def chiletrabajos():
         offers.append(data)
 
         time.sleep(10)
-
+    print('Step 2: Get all offers - OK')
     # Creamos un archivo json de las ofertas.
     now = datetime.now()
     filename = "chiletrabajos-" + now.strftime("%d-%m-%Y %H:%M:%S") + ".json"
@@ -157,6 +157,7 @@ def chiletrabajos():
         content=result,
         branch="master"
     )
+    print('3 STEP: Push to github - OK')
 
 
 def laborum():
@@ -175,7 +176,7 @@ def laborum():
 
     content = driver.page_source
     soup = BeautifulSoup(content, "html.parser")
-    print('STEP 1')
+
     # Seleccionamos el número de paginación inicial y final
     first_pag = soup.find('button', attrs={'class': 'gENuBC'}).text.strip()
     last_pag = soup.find('button', attrs={'class': 'eDchar'})
@@ -208,15 +209,14 @@ def laborum():
 
       time.sleep(10)
       first_pag = first_pag + 1
-    print('STEP 2')
+    print('STEP 1: Get all links - OK')
     # Obtenemos toda la información de cada oferta
     for link in links:
       driver.get("https://www.laborum.cl" + str(link))
 
       data = {}
-      print('STEP 2.1')
+ 
       try:
-          print('STEP 2.1.1')
           WebDriverWait(driver, 10).until(
               EC.presence_of_element_located((By.CLASS_NAME, 'dmYIZt')))
           content = driver.page_source
@@ -224,7 +224,10 @@ def laborum():
 
           title = soup.find('h1', attrs={'class': 'dmYIZt'})
           if title is not None:
-              data['title'] = title.text.strip()
+              if title.text.strip() == "No encontramos la página que buscás":
+                  continue
+              else:
+                  data['title'] = title.text.strip()
 
           company = soup.find('h2', attrs = {'class': 'bkFDjf'})
           if company is not None:
@@ -250,8 +253,8 @@ def laborum():
     
       offers.append(data)
       time.sleep(10)
-    print('STEP 3')
-    print(len(offers))
+    print('STEP 2: Get all offers - OK')
+
     # Creamos un archivo json de las ofertas.
     now = datetime.now()
     filename = "laborum-" + now.strftime("%d-%m-%Y %H:%M:%S") + ".json"
@@ -271,10 +274,186 @@ def laborum():
         content=result,
         branch="master"
     )
+    print('STEP 3: Push to github - OK')
+
+def bne():
+    print('RUNNING BNE')
+    # Abrimos el navegador Chrome
+    driver = webdriver.Chrome(executable_path=os.environ.get(
+        "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+    # Obtenemos la fecha actual del sistema
+    today = str(date.today())
+    today = today.split('-')
+
+    # Inicializamos las variables
+    day = today[2]
+    month = today[1]
+    year = today[0]
+    firstPage = "https://www.bne.cl/ofertas?mostrar=empleo&fechaIniPublicacion=" + day + "%2F" + month + "%2F" + year + "&numPaginaRecuperar=1&numResultadosPorPagina=10&clasificarYPaginar=true"
+    offers = []
+    links = []
+    data = {}
+
+    # Abrimos chrome
+    driver.get(firstPage)
+
+    content = driver.page_source
+    soup = BeautifulSoup(content, 'html.parser')
+
+    # Seleccionamos el número de paginacion inicial y final.
+    paginations = soup.findAll('a', attrs = {'data-pagina': True})
+    init = 0
+    finish = 0
+
+    if (len(paginations) > 0):
+        if (len(paginations) == 1):
+            init = int(paginations[0].get('data-pagina'))
+            finish = init
+        else:
+            init = int(paginations[0].get('data-pagina'))
+            finish = int(paginations[-1].get('data-pagina'))
+
+    # Implementar while desde la primera pagina, hasta la ultima
+    while init <= finish:
+        driver.get("https://www.bne.cl/ofertas?mostrar=empleo&fechaIniPublicacion=" + day + "%2F" + month + "%2F" + year + "&numPaginaRecuperar=" + str(init) + "&numResultadosPorPagina=10&clasificarYPaginar=true")
+
+        try:
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'resultadoOfertas')))
+            print("Page is ready")
+            content = driver.page_source
+            soup = BeautifulSoup(content, "html.parser")
+            
+            for col in soup.findAll('article', attrs = {'class': 'resultadoOfertas'}):
+                for a in col.findAll('a'):
+                    if a is not None:
+                        tmp_link = str(a.get('href'))
+                        if (tmp_link.find('oferta') == 1):
+                            if tmp_link not in links:
+                                links.append(tmp_link)
+                            
+        except TimeoutException:
+            print("Looking for much time..")
+    
+        init = init + 1
+        time.sleep(7)
+    print('Step 1: Get all links - OK')
+    
+    # Ingresamos a cada oferta laboral
+    for offer in links:
+        driver.get("https://www.bne.cl" + str(offer))
+    
+        data = {}
+        tmp_title = []
+    
+        try:
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'width1000')))
+            print("Page is ready")
+            content = driver.page_source
+            soup = BeautifulSoup(content, "html.parser")
+        
+            title = soup.find('div', attrs = {'class': 'titulo'})
+            tmp_title = title.findAll('span')
+        
+            if (len(tmp_title) > 2):
+                title_name = str(tmp_title[0].text.strip()).split(')')
+                data['Titulo'] = tmp_title[2].text.strip()
+                data['Ocupacion'] = title_name[-1].strip()
+                vacancies = tmp_title[2].text.strip().split('(')
+                vacancies = vacancies[-1].split(' ')
+                data['Vacantes'] = int(vacancies[0])
+            else:
+                title_name = str(tmp_title[0].text.strip()).split(')')
+                data['Titulo'] = tmp_title[1].text.strip()
+                data['Ocupacion'] = title_name[-1].strip()
+                vacancies = tmp_title[1].text.strip().split('(')
+                vacancies = vacancies[-1].split(' ')
+                data['Vacantes'] = int(vacancies[0])
+            
+            tmp_array = []
+
+            for panel in soup.findAll('article', attrs = {'class': 'panel'}):
+                title_category = panel.find('h3', attrs = {'panel-title'})
+                if title_category is not None:
+                    title_category = title_category.text.strip()
+                    if title_category == 'DATOS DE CONTACTO':
+                        for item in panel.findAll('span'):
+                            if item is not None:
+                                text = item.text.strip()
+                                tmp_array.append(text)
+                    
+                            if len(tmp_array) == 2:
+                                tmp_array[0] = str(tmp_array[0]).split(':')[0]
+                                data[tmp_array[0]] = tmp_array[-1]
+                                tmp_array = []
+        
+                    if title_category == 'DESCRIPCIÓN':
+                        description = panel.find('p').text.strip()
+            
+                        items = panel.findAll('span')
+                        data['Ubicacion'] = items[0].text.strip()
+                        data['Fecha'] = items[1].text.strip()
+                        salary = str(items[3].text.strip()).replace(" ", "").replace(u'\xa0', u' ')
+                        data['Salario'] = salary.replace(u'\xa0', u' ')
+                        data['Tipo'] = items[-1].text.strip()
+            
+                    if title_category == 'REQUISITOS SOLICITADOS':
+                        for item in panel.findAll('span'):
+                            if item is not None:
+                                text = item.text.strip()
+                                tmp_array.append(text)
+                    
+                            if len(tmp_array) == 2:
+                                tmp_array[0] = str(tmp_array[0]).split(':')[0]
+                                data[tmp_array[0]] = tmp_array[-1]
+                                tmp_array = []
+                    
+                    if title_category == 'CARACTERÍSTICAS':
+                        for item in panel.findAll('span'):
+                            if item is not None:
+                                text = item.text.strip()
+                                tmp_array.append(text)
+                    
+                            if len(tmp_array) == 2:
+                                tmp_array[0] = str(tmp_array[0]).split(':')[0]
+                                data[tmp_array[0]] = tmp_array[-1]
+                                tmp_array = []
+                    
+                        prof = panel.find('label')
+                        if prof is not None:
+                            data['Oferta de tipo práctica profesional'] = prof.text.strip()
+
+        except TimeoutException:
+            print("Looking for much time..")
+    
+        offers.append(data)
+        time.sleep(5)
+    print('Step 2: Get all offers - OK')
+    # Creamos un archivo json de las ofertas.
+    now = datetime.now()
+    filename = "bne-" + now.strftime("%d-%m-%Y %H:%M:%S") + ".json"
+    result = json.dumps(offers, ensure_ascii=False)
+
+    # Enviamos el archivo creado a github
+    token = "29e578fceb764670077aee08fb42dba51b7eb744"
+
+    repo = "FavazCL/WS-Ofertas"
+
+    g = Github(token)
+
+    repo = g.get_repo(repo)
+    repo.create_file(
+        path='bne/' + filename,
+        message="Se agregaron nuevas ofertas desde: " + filename,
+        content=result,
+        branch="master"
+    )
+    print('Step 3: Push to github - OK')
 
 # Señalamos que se ejecute todos los días a la hora fijada.
 schedule.every().day.at('19:30').do(chiletrabajos)
-schedule.every().day.at('22:50').do(laborum)
+schedule.every().day.at('22:30').do(laborum)
+schedule.every().day.at('23:30').do(bne)
 
 while True:
     schedule.run_pending()
